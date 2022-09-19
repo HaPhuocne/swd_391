06c39e01ff1871b999dcb0cdff6a46234a38a24b
HaPhuoc <haphuoc140203@gmail.com>
2022-09-19 09:43:01 +0700

    Test CI/CD

---
+++ src/main/java/com/fpt/swd391/fall2022/swd91/Swd91Application.java
@@ -0,0 +1,13 @@
+package com.fpt.swd391.fall2022.swd91;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swd91Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Swd91Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/fpt/swd391/fall2022/swd91/Swd91ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.swd391.fall2022.swd91;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swd91ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
