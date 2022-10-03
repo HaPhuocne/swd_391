cfd6ba3b8c4b7932c0fbc3df683c88715a2e8972
HaPhuoc <haphuoc140203@gmail.com>
2022-10-03 07:39:35 +0700

    CrssConfiguration Be to FE

---
+++ src/main/java/com/fpt/swd391/fall2022/swd391/config/CorsConfiguration.java
@@ -0,0 +1,21 @@
+package com.fpt.swd391.fall2022.swd391.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration 
+{
+    @Bean
+    public WebMvcConfigurer corsConfigurer() 
+    {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("https://be-c2c.herokuapp.com/","http://localhost:3000/","https://be-c2c.herokuapp.com/*");
+            }
+        };
+    }
+}
