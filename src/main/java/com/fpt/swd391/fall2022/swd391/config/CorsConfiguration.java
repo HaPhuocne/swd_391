d5357c4e5f08dc03c4736715027a74639d41e136
HaPhuoc <haphuoc140203@gmail.com>
2022-10-09 16:01:14 +0700

    test

--- src/main/java/com/fpt/swd391/fall2022/swd391/config/CorsConfiguration.java
+++ src/main/java/com/fpt/swd391/fall2022/swd391/config/CorsConfiguration.java
@@ -6,15 +6,16 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfiguration 
+public class CorsConfiguration
 {
     @Bean
-    public WebMvcConfigurer corsConfigurer() 
+    public WebMvcConfigurer corsConfigurer()
     {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("https://be-c2c.herokuapp.com/","http://localhost:3000/","https://be-c2c.herokuapp.com/*");
+                registry.addMapping("/**").allowedOrigins("https://be-c2c.herokuapp.com/"
+                        ,"http://localhost:3000/","https://be-c2c.herokuapp.com/*");
             }
         };
     }
