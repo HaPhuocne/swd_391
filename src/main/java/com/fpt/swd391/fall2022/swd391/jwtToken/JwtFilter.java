261b5c3e4baefe9fe8bd44406b4286a315b7bff7
phamdanghai <haipham20002000@gmail.com>
2022-10-12 23:11:40 +0700

    update validation

--- src/main/java/com/fpt/swd391/fall2022/swd391/jwtToken/JwtFilter.java
+++ src/main/java/com/fpt/swd391/fall2022/swd391/jwtToken/JwtFilter.java
@@ -1,6 +1,7 @@
 package com.fpt.swd391.fall2022.swd391.jwtToken;
 
 
+import com.fpt.swd391.fall2022.swd391.entity.Account;
 import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
